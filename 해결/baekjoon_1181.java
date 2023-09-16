dba1d56ef438c5160e5ef00e9ee2df728d6ef43a
redbuttonking <103170787+redbuttonking@users.noreply.github.com>
2023-09-16 16:34:04 +0900

    comment 수정

--- 해결/baekjoon_1181.java
+++ 해결/baekjoon_1181.java
@@ -1,4 +1,4 @@
-/*
+/* comment
 - LinkedHashSet 에 대해 알게 됨
 - 초기 값인 배열( 정렬하고 싶은 배열 )을 중복을 제거하고 동시에 정렬까지 보장된 LinkedHashSet를 만듦
 - sort()에서 Comparator.comparing(String::length)를 통해 배열에 있는 문자열 길이 순으로 정렬을 함 
@@ -23,13 +23,15 @@ public class baekjoon_1181 {
       word[i] = scan.next();
     }
     
-    Arrays.sort(word);
+    // 사전 순으로 단어 정렬
+    Arrays.sort(word); 
 
-    LinkedHashSet<String> word2 = new LinkedHashSet<String>(Arrays.asList(word)); 
     // 중복을 제거하며 정렬(순서)보장 , 선언과 동시에 초기 값 설정 
+    LinkedHashSet<String> word2 = new LinkedHashSet<String>(Arrays.asList(word)); 
     
     word = word2.toArray(new String[0]);
 
+    // 문자열 길이 순으로 정렬
     Arrays.sort(word,Comparator.comparing(String::length));
     
     // System.out.println(Arrays.deepToString(word)); // 배열 값들 문자열로 보여주기
